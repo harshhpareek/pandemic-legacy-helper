d2eea3e81b641077b0b4309e39806d25f5a3bc8f
Harsh Pareek <harshhpareek@gmail.com>
2022-09-04 00:16:04 -0700

    Added Infection Card type with city names

--- src/types.tsx
+++ src/types.tsx
@@ -9,3 +9,10 @@ export interface TState {
 
 // For the player deck, we will not track individual player cards, just the colour
 export type TPlayerCard = '_' | 'Epidemic' | 'Black' | 'Yellow' | 'Blue' | 'Red' | 'Funded'
+
+// For the Infection Deck, we will track each card
+export type TBlueInfectionCard = 'Atlanta' | 'Chicago' | 'Essen' | 'London' | 'Madrid' | 'Milan' | 'Montreal' | 'New York' | 'Paris' | 'San Francisco' | 'St. Petersburg' | 'Washington'
+export type TBlackInfectionCard = 'Algiers' | 'Baghdad' | 'Cairo' | 'Chennai' | 'Delhi' | 'Istanbul' | 'Karachi' | 'Kolkata' | 'Moscow' | 'Mumbai' | 'Riyadh' | 'Tehran'
+export type TYellowInfectionCard = 'Bogota' | 'Buenos Aries' | 'Johannesburg' | 'Khartoum' | 'Kinshasa' | 'Lagos' | 'Lima' | 'Los Angeles' | 'Mexico City' | 'Miami' | 'Santiago' | 'Sao Paulo'
+export type TRedInfectionCard = 'Bangkok' | 'Beijing' | 'Ho Chi Minh City' | 'Hong Kong' | 'Jakarta' | 'Manila' | 'Osaka' | 'Seoul' | 'Shanghai' | 'Sydney' | 'Taipei' | 'Tokyo'
+export type TInfectionCard = TBlueInfectionCard | TBlackInfectionCard | TYellowInfectionCard | TRedInfectionCard
