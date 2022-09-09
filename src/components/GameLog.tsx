49e9f6b4ed612e136d947ca3b828e7977b855b79
Harsh Pareek <harshhpareek@gmail.com>
2022-09-08 21:12:53 -0700

    Rendered Game Log

--- src/components/GameLog.tsx
+++ src/components/GameLog.tsx
@@ -1,6 +1,6 @@
-import { ListItem, ListItemText, Avatar, ListItemAvatar, ListItemButton, ListItemIcon } from '@mui/material'
+import { ListItem, ListItemText, Avatar, ListItemAvatar, FormControl, MenuItem, Select } from '@mui/material'
 import * as React from 'react'
-import { infectionCardColor, playerCardIcon, TState } from '../types'
+import { AllCities, infectionCardColor, playerCardIcon, PlayerCardTypes, TState } from '../types'
 import { stringAvatar } from './DraggableAvatarStack'
 import InitialInfectionsLog from './InitialInfections'
 import InitialPlayerCardsLog from './InitialPlayerCards'
@@ -12,30 +12,93 @@ interface GameLogProps {
 
 export default class GameLog extends React.Component<GameLogProps, {}> {
   render (): React.ReactNode {
+    const players = this.props.parentState.players
+    const playerColors = this.props.parentState.playerColors
+    const history = this.props.parentState.history
     return (<>
             <InitialInfectionsLog parentState={this.props.parentState} setParentState={this.props.setParentState}></InitialInfectionsLog>
             <InitialPlayerCardsLog parentState={this.props.parentState} setParentState={this.props.setParentState}></InitialPlayerCardsLog>
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar {...stringAvatar('Harsh Pareek', 'Blue')} />
-                </ListItemAvatar>
-                <ListItemText primary="drew" sx={{ color: 'blue' }} />
-                <ListItemButton sx={{ width: '2%' }}>
-                    <ListItemIcon>{playerCardIcon('Epidemic')} </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton sx={{ width: '2%' }}>
-                    <ListItemIcon>{playerCardIcon('Red')} </ListItemIcon>
-                </ListItemButton>
-                <ListItemText primary="and" />
-                <ListItemText primary="infected" sx={{ color: 'green' }} />
-                <ListItemButton>
-                    <ListItemText primary='Kinshasa' sx={{ color: infectionCardColor('Kinshasa') }} />
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemText primary='Algiers' sx={{ color: infectionCardColor('Algiers') }} />
-                </ListItemButton>
-            </ListItem>
-        </>
-    )
+            {history.map((row, turnIdx) => {
+              return (
+                    <React.Fragment key={turnIdx}>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar {...stringAvatar(players[row.player_id], playerColors[row.player_id])} />
+                            </ListItemAvatar>
+                            <ListItemText primary="drew" sx={{ color: 'blue' }} />
+                            {row.playerCards.map((handCard, handCardIdx) => {
+                              return (
+                                    <FormControl
+                                        key={handCardIdx}
+                                        sx={{
+                                          minWidth: 60,
+                                          alignItems: 'center',
+                                          '& .MuiOutlinedInput-notchedOutline': {
+                                            borderWidth: '0 !important'
+                                          }
+                                        }}
+                                    ><Select
+                                        value={handCard}
+                                        onChange={(event) => {
+                                          console.log(event)
+                                          this.props.setParentState(
+                                            (current: TState) =>
+                                              ({
+                                                ...current
+                                                //   initialPlayerCards:
+                                                //   initialPlayerCards.map((hand, k) =>
+                                                //     (k === playerId
+                                                //       ? (handCardIdx === 0 ? [event.target.value, hand[1]] : [hand[0], event.target.value])
+                                                //       : hand))
+                                              }))
+                                        }}
+                                        IconComponent={() => null}
+                                        inputProps={
+                                            {
+                                              sx: {
+                                                padding: '0 !important'
+                                              }
+                                            }}
+                                    >
+                                            {PlayerCardTypes.map((card, j) => {
+                                              return (
+                                                    <MenuItem key={j} value={card}>{playerCardIcon(card)}</MenuItem>)
+                                            })}
+                                        </Select>
+                                    </FormControl>)
+                            })
+                            }
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText inset
+                                primary={'infected'}
+                                sx={{ color: 'green' }} />
+
+                            {row.infectionCards.map((card, j) => {
+                              return (
+                                    <FormControl key={j}>
+                                        <Select
+                                            value={card}
+                                            sx={{ color: infectionCardColor(card) }}
+                                            onChange={(event) => {
+                                              this.props.setParentState(
+                                                (current: TState) =>
+                                                  ({
+                                                    ...current
+                                                    // initialInfections:
+                                                    // infections.map((inf, k) => (k === i ? event.target.value : inf))
+                                                  }))
+                                            }}
+                                        >
+                                            {AllCities.map((city, j) => {
+                                              return (
+                                                    <MenuItem key={j} sx={{ color: infectionCardColor(city) }} value={city}>{city}</MenuItem>)
+                                            })}
+                                        </Select >
+                                    </FormControl>)
+                            })}
+                        </ListItem>
+                    </React.Fragment>)
+            })}</>)
   }
 }
