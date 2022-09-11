b15d4a6e3579fdec7b858c6dae6ccdb766be5423
Harsh Pareek <harshhpareek@gmail.com>
2022-09-10 19:37:32 -0700

    Added Player and infection card counting

--- src/components/Inferences.tsx
+++ src/components/Inferences.tsx
@@ -1,37 +1,82 @@
+import { Alert } from '@mui/material'
 import * as React from 'react'
-import { TState, TPlayerCard } from '../types'
+import { TState, TPlayerCard, TInfectionCard } from '../types'
 
 interface InferencesProps {
   parentState: TState
 }
 
-interface TInferencesState {
-  cardCounts: Map<TPlayerCard, number>
-}
+export default class InitialInfectionsLog extends React.Component<InferencesProps, {}> {
+  numEpidemic: number = 0
+  epidemicPositions: number[] = []
+  alerts: string[] = []
+  playerCardCounts: Map<TPlayerCard, number> = new Map<TPlayerCard, number>()
+  pastInfectionCardStacks: TInfectionCard[][] = []
+  infectionCardsOnTop: TInfectionCard[] = []
 
-function countCards (state: TState): Map<TPlayerCard, number> {
-  const counts = new Map<TPlayerCard, number>()
-  state.initialPlayerCards.forEach(hand => {
-    hand.forEach(card => {
-      counts.set(card, (counts.get(card) ?? 0) + 1)
+  countPlayerCards (): void {
+    const counts = new Map<TPlayerCard, number>()
+    let playerDeckPosition: number = 0
+    this.props.parentState.initialPlayerCards.forEach(hand => {
+      hand.forEach(card => {
+        if (card === 'Epidemic') {
+          this.alerts.push('Epidemic cannot happen in initially dealt cards')
+        }
+        counts.set(card, (counts.get(card) ?? 0) + 1)
+      })
     })
+
+    this.props.parentState.history.forEach(({ playerCards }) => {
+      playerCards.forEach(card => {
+        playerDeckPosition++
+        if (card === 'Epidemic') {
+          this.numEpidemic += 1
+          this.epidemicPositions.push(playerDeckPosition)
+        }
+        counts.set(card, (counts.get(card) ?? 0) + 1)
+      })
+    })
+    this.playerCardCounts = counts
   }
-  )
-  return counts
-}
 
-export default class InitialInfectionsLog extends React.Component<InferencesProps, TInferencesState> {
-  constructor (props: InferencesProps) {
-    super(props)
+  trackInfectionCards (): void {
+    this.infectionCardsOnTop = []
+    this.props.parentState.initialInfections.forEach(card => {
+      if (card === '_') {
+        return
+      }
+      this.infectionCardsOnTop.push(card)
+    })
 
-    this.state = {
-      cardCounts: countCards(props.parentState)
-    }
+    this.props.parentState.history.forEach(({ playerCards, infectionCards }) => {
+      if (playerCards.includes('Epidemic')) {
+        // The cards drawn after epidemic
+        this.pastInfectionCardStacks.push(this.infectionCardsOnTop)
+        this.infectionCardsOnTop = []
+      }
+      infectionCards.forEach(card => {
+        if (card === '_') {
+          return
+        }
+        this.infectionCardsOnTop.push(card)
+      })
+    })
   }
 
   render (): React.ReactNode {
+    this.numEpidemic = 0
+    this.alerts = []
+
+    this.countPlayerCards()
+    this.trackInfectionCards()
     return (<>
-            <pre>{JSON.stringify(Object.fromEntries(countCards(this.props.parentState)), null, 2)}</pre>
-        </>)
+      {this.alerts.map((alert, i) => <Alert key={i} severity="error">{alert}</Alert>)}
+      <h4>Infection Cards</h4>
+      <pre>{JSON.stringify(this.pastInfectionCardStacks, null, 2)}</pre>
+      <p></p>
+      <pre>{JSON.stringify(this.infectionCardsOnTop)}</pre>
+      <h4>Player Card counts</h4>
+      <pre>{JSON.stringify(Object.fromEntries(this.playerCardCounts), null, 2)}</pre>
+    </>)
   }
 }
