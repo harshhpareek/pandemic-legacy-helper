6a50bb6368c575066b37711e697584dec82bfc7f
Harsh Pareek <harshhpareek@gmail.com>
2022-09-25 01:53:45 -0700

    Removed palette. Not using it

--- src/theme.ts
+++ src/theme.ts
@@ -1,22 +1,7 @@
 
-import { red } from '@mui/material/colors'
 import { createTheme, ThemeOptions } from '@mui/material/styles'
 // A custom theme for this app
 const theme: ThemeOptions = {
-  palette: {
-    primary: {
-      main: '#556cd6'
-    },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: '#fff'
-    }
-  },
   components: {
     MuiContainer: {
       defaultProps: {
